775431b8068c52b776f16513b7855dcf9416da27
Adil <adil.kenesbek21@gmail.com>
2021-04-15 22:03:07 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lab10';
+  title = 'Template driven forms';
+
+
+  countryList: country[] = [
+    new country('1', 'KZ'),
+    new country('2', 'UK'),
+    new country('3', 'USA')
+  ];
+
+  contact: contact;
+
+  ngOnInit() {
+
+    this.contact = {
+      firstname: 'Name',
+      lastname: 'Surname',
+      email: 'email@gmail.com',
+      gender: 'male',
+      isMarried: true,
+      country: '2',
+      address: {
+        city: 'London',
+        street: 'Oxford Rd',
+        pincode: 'M139JS'
+      }
+    };
+
+  }
+
+  onSubmit() {
+    console.log(this.contact);
+  }
+
+  setDefaults() {
+    this.contact = {
+      firstname: 'Name',
+      lastname: 'Surname',
+      email: 'email@gmail.com',
+      gender: 'male',
+      isMarried: true,
+      country: '2',
+      address: {
+        city: 'London',
+        street: 'Oxford Rd',
+        pincode: 'M139JS'
+      }
+    };
+  }
+
+  changeCountry() {
+    this.contact.country = "1";
+  }
+
+  reset(contactForm: NgForm) {
+    contactForm.resetForm();
+  }
+}
+
+export class contact {
+  firstname: string;
+  lastname: string;
+  email: string;
+  gender: string;
+  isMarried: boolean;
+  country: string;
+  address: {
+    city: string;
+    street: string;
+    pincode: string;
+  };
+}
+
+
+export class country {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
